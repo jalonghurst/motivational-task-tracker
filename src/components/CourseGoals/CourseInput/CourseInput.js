ba38a3214ac4c194199c7e1dfd7024c5f9f2bb58
jalonghurst <jacqui.longhurst@btinternet.com>
2021-11-14 15:35:28 +0100

    removing inline styles

--- src/components/CourseGoals/CourseInput/CourseInput.js
+++ src/components/CourseGoals/CourseInput/CourseInput.js
@@ -26,12 +26,8 @@ const CourseInput = props => {
   return (
     <form onSubmit={formSubmitHandler}>
       <div className="form-control">
-        <label style={{ color: !isValid ? "red" : "black" }}>Course Goal</label>
+        <label>Course Goal</label>
         <input
-          style={{
-            borderColor: !isValid ? "red" : "#ccc",
-            background: !isValid ? "salmon" : "transparent",
-          }}
           type="text"
           onChange={goalInputChangeHandler}
         />
