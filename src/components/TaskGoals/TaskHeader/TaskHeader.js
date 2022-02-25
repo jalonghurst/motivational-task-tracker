397efe67933e1a95aabab922807c04bcecd493b3
unknown <jacqui.longhurst@btinternet.com>
2022-02-25 08:11:23 +0100

    Added ternary logic to toggle button for styling, but text is not showing

--- src/components/TaskGoals/TaskHeader/TaskHeader.js
+++ src/components/TaskGoals/TaskHeader/TaskHeader.js
@@ -1,12 +1,15 @@
 import React from 'react'
 import Button from '../../UI/Button/Button'
-import TaskHeader from './TaskHeader/TaskHeader.css'
+import '../TaskHeader/TaskHeader.css'
 
 const TaskHeader = (props) => {
   return (
     <header>
         <h1>My Task Tracker</h1>
-        <Button onClick={props.onAddGoal}>Add New Task</Button>
+        <Button 
+        color={props.onAddGoal ? '#09325c': 'green'} 
+        text={props.onAddGoal ? 'Hide Form' : 'Add New Task'} 
+        onClick={props.onAddGoal}/>
    </header>
   )
 }
