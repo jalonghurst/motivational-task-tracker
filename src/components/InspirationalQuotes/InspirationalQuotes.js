07b8ddfbddbf37c9ed0c5a0441b0fa824c602d72
unknown <jacqui.longhurst@btinternet.com>
2022-03-02 07:05:10 +0100

    New folder inspirationalQuotes, and js file

---
+++ src/components/InspirationalQuotes/InspirationalQuotes.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+
+const InspirationalQuotes = () => {
+  return (
+    <div className="quote-container"> 
+        <section>
+         <h2> "There is nothing impossible to they who will try."
+— Alexander the Great </h2>
+   </section>
+   </div>
+  )
+}
+
+export default InspirationalQuotes
